e66ef2ac58f279948f3a9cf396527e9e1412bec3
jnphilipp <jnphilipp@gmail.com>
2014-05-05 15:48:38 +0200

    RSS parser.

--- openLegislature/src/main/java/org/openlegislature/App.java
+++ openLegislature/src/main/java/org/openlegislature/App.java
@@ -1,7 +1,17 @@
 package org.openlegislature;
 
+import com.sun.syndication.feed.synd.SyndEntry;
+import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.io.FeedException;
+import com.sun.syndication.io.SyndFeedInput;
+import com.sun.syndication.io.XmlReader;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import org.apache.commons.validator.routines.UrlValidator;
 import org.openlegislature.util.Helpers;
 
 /**
@@ -11,6 +21,7 @@ import org.openlegislature.util.Helpers;
  */
 public class App {
 	private static final String BUNSTAG_PROTOKOLL_URL = "http://dip21.bundestag.de/dip21/btp/{period}/{period}{session}.pdf";
+	private static final String BUNSTAG_PROTOKOLL_RSS = "http://www.bundestag.de/rss_feeds/plenarprotokolle.rss";
 
 	public static void main(String[] args) {
 		Logger.getInstance();
@@ -45,20 +56,52 @@ public class App {
 			String file = path + "/" + p + s +".pdf";
 			String url = App.BUNSTAG_PROTOKOLL_URL.replaceAll("\\{period\\}", p).replaceAll("\\{session\\}", s);
 
-			try {
-				Logger.debug("Downloding protokol: " + p + s);
-				Helpers.saveURLToFile(url, file);
+			if ( !new UrlValidator().isValid(url) )
 				session++;
-			}
-			catch ( IOException e ) {
-				period++;
-				session = 1;
+			else {
+				try {
+					Logger.debug("Downloding protokol: " + p + s);
+					Helpers.saveURLToFile(url, file);
+					session++;
+				}
+				catch ( IOException e ) {
+					period++;
+					session = 1;
+				}
 			}
 		}
 	}
 
-	private static void checkBundestagRSS() {
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	private static List<String> checkBundestagRSS() {
+		List<String> protocols = new ArrayList<>();
+
+		try {
+			XmlReader xmlReader = new XmlReader(new URL(App.BUNSTAG_PROTOKOLL_RSS));
+			SyndFeed syndFeed = new SyndFeedInput().build(xmlReader);
+
+			Collection<?> syndEntries = syndFeed.getEntries();
+			for ( Object entry : syndEntries ) {
+				if ( !new UrlValidator().isValid(((SyndEntry)entry).getLink()) )
+					continue;
+
+				String name = new File(((SyndEntry)entry).getLink()).getName();
+
+				Logger.debug("Protocol: " + name);
+
+				String period = name.substring(0, 2);
+				String file = Helpers.getUserDir() + "/data/bundestag/" + period + "/" + name;
+				if ( !new File(file).exists() ) {
+					new File(file).getParentFile().mkdirs();
+					Helpers.saveURLToFile(((SyndEntry)entry).getLink(), file);
+					protocols.add(file);
+				}
+			}
+		}
+		catch ( IllegalArgumentException | IOException | FeedException e ) {
+			Logger.error(e.toString());
+		}
+
+		return protocols;
 	}
 
 	private static void downloadAllBundesrat() {
