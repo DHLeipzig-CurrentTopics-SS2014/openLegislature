c6000b7e34856fd4122cec83e7748faa8e008408
jnphilipp <jnphilipp@gmail.com>
2014-07-02 11:01:12 +0200

    Tokenizer from pgsql branch.

---
+++ openLegislature/src/main/java/org/openlegislature/nlp/tokenization/Tokenizer.java
@@ -0,0 +1,14 @@
+package org.openlegislature.nlp.tokenization;
+
+/**
+ *
+ * @author jnphilipp
+ * @version 0.0.1
+ */
+public class Tokenizer {
+	public static String[] tokenize(String text) {
+		String s = text.replaceAll("[!\"§$%&/\\(\\)=?\\\\\\}\\]\\[\\{+*#';,.:_<>|]", " ");
+		s = s.replaceAll("\n", " ").replaceAll("\\s\\s+", " ");
+		return s.split("\\s+");
+	}
+}
